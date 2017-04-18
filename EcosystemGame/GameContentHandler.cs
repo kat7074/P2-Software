58f41ecf0ae8b7a83c12cf6ebd75b151ebab2e94
Jonas Alsen <grann.alsen@gmail.com>
2017-04-18 16:47:05 +0200

    FloraFaunaHandler og ResourceHandler tilføjet som en privat underklasse af GameContentHandler

--- EcosystemGame/GameContentHandler.cs
+++ EcosystemGame/GameContentHandler.cs
@@ -6,16 +6,33 @@ using System.Threading.Tasks;
 
 namespace EcosystemGame
 {
-    //The class used to load and unload content described by the start parameters of a LevelItem
-    class GameContentHandler
+    // The class used to load and unload content described by the start parameters of a LevelItem.
+    public class GameContentHandler
     {
-        //LoadContent will be executed when a level has been selected from the GameMenu
+        // LoadContent will be executed when a level has been selected from the GameMenu.
         public void LoadContent()
         {
 
         }
-        //UnloadContent will be executed when a level has been exited or completed, as the game returns to the GameMenu
+
+        // UnloadContent will be executed when a level has been exited or completed, as the game returns to the GameMenu.
         public void UnloadContent()
+        {
+
+        }
+
+        // UpdateContent will be used when a turn ends, to update all the different variables depending on what actons have been taken.
+        public void UpdateContent()
+        {
+
+        }
+
+        private class FloraFaunaHandler
+        {
+
+        }
+
+        private class ResourceHandler
         {
 
         }
