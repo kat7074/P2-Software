c876bb123eb0f15dd8ff75b111c79d30d8c748c6
Jonas Alsen <grann.alsen@gmail.com>
2017-04-18 14:27:46 +0200

    Coding conventions overholdt.

--- EcosystemGame/FloraFaunaBase.cs
+++ EcosystemGame/FloraFaunaBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EcosystemGame
 {
-    //The base class that FloraItem and FaunaItem will inherit from
+    // The base class that FloraItem and FaunaItem will inherit from.
     public abstract class FloraFaunaBase
     {
 
