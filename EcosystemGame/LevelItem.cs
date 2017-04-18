1fa941a757c9e5dbe1dae38b1548ab92a336af2e
Jonas Alsen <grann.alsen@gmail.com>
2017-04-18 13:28:28 +0200

    Lidt ændringer til Levelitem og LevelItemBase, de er stadig begge i testfasen

--- EcosystemGame/LevelItem.cs
+++ EcosystemGame/LevelItem.cs
@@ -6,25 +6,29 @@ using System.Threading.Tasks;
 
 namespace EcosystemGame
 {
-    //The base class that LevelItem will inherit from
-    public class LevelItem : ILevel
+    // Holds all the different LevelItems
+    public class LevelItems
     {
-        public string gameObjective { get { return "Add 5 Flora and Fauna to the system"; } }
-        public int startNutrients { get { return 1; } }
-        public int startSeason { get { return 1; } }
 
-        public List<List<FaunaItem>> startFauna { get { return new List<List<FaunaItem>>(); } }
-        public List<List<FloraItem>> startFlora { get { return new List<List<FloraItem>>(); } }
-
-        public bool CheckObjective()
+        public class LevelItemOne : ILevel
         {
-            // Checks if the objective is complete. Replace (true) with the correct parameters for the objective. 
-            if (true)
+            public string gameObjective { get { return "Add 5 Flora and Fauna to the system"; } }
+            public int startNutrients { get { return 1; } }
+            public int startSeason { get { return 1; } }
+
+            public List<List<FaunaItem>> startFauna { get { return new List<List<FaunaItem>>(); } }
+            public List<List<FloraItem>> startFlora { get { return new List<List<FloraItem>>(); } }
+
+            public bool CheckObjective()
             {
-                return true;
+                // Checks if the objective is complete. Replace (true) with the correct parameters for the objective. 
+                if (true)
+                {
+                    return true;
+                }
+                else
+                    return false;
             }
-            else
-                return false;
         }
     }
 }
