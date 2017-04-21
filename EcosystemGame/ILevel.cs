fde520ca7a61b6d5b410df8db1faab8e31c87051
Jonas Alsen <grann.alsen@gmail.com>
2017-04-21 10:18:12 +0200

    Små rettelser

--- EcosystemGame/ILevel.cs
+++ EcosystemGame/ILevel.cs
@@ -14,8 +14,8 @@ namespace EcosystemGame
         int startSeason { get; }
 
         // Lists containing a list of the differet species of flora and fauna the level starts with.
-        List<List<FloraItem>> startFauna { get; }
-        List<List<FloraItem>> startFlora { get; }
+        List<List<FloraFaunaItem>> startFauna { get; }
+        List<List<FloraFaunaItem>> startFlora { get; }
 
         // Method to check if the objective has been completed.
         bool CheckObjective();
